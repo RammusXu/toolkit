602cc2f72ed3f3c1d6753cd6f5de475283249191
Rammus Xu <comte_ken@hotmail.com>
2019-06-05 14:37:28 +0800

    demo python gevent

---
+++ demo/python/gevent/demo.py
@@ -0,0 +1,27 @@
+import gevent
+from gevent import monkey
+import random
+import time
+monkey.patch_all()
+
+
+def task(pid):
+    """
+    Some non-deterministic task
+    """
+    time.sleep(random.randint(0,2)*0.001)
+    print('Task', pid, 'done')
+
+def synchronous():
+    for i in range(1,10):
+        task(i)
+
+def asynchronous():
+    threads = [gevent.spawn(task, i) for i in range(10)]
+    gevent.joinall(threads)
+
+print('Synchronous:')
+synchronous()
+
+print('Asynchronous:')
+asynchronous()
