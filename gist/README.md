e8c8cf5ae48ccb6a1947dd005fa97bd798b9d22c
Rammus Xu <comte_ken@hotmail.com>
2020-06-18 16:12:46 +0800

    install runner

---
+++ gist/README.md
@@ -0,0 +1,5 @@
+## How to use this scripts
+```bash
+gist=https://raw.githubusercontent.com/RammusXu/toolkit/master/gist/install/github-action-runner.sh
+curl -sL $gist | bash - 
+```
--- gist/install/github-action-runner.sh
+++ gist/install/github-action-runner.sh
@@ -4,3 +4,23 @@
 adduser runner --disabled-password  --gecos ""
 echo "runner ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers &&\
 usermod -aG sudo runner
+
+## Pre-installed softwares
+curl -sL https://deb.nodesource.com/setup_12.x | bash - &&\
+    apt-get install -y nodejs
+
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - &&\
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list &&\
+    apt-get update && apt-get install -y yarn
+
+apt-get install -y --no-install-recommends iputils-ping git
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+usermod -aG docker runner
+
+## Install action-runner
+su runner
+curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.263.0.tar.gz
+sudo ./bin/installdependencies.sh
